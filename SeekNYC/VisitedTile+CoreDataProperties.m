e624c4402994654f4789fdf86f939f20618fbdd7
Justine Gartner <GartnerJustine@gmail.com>
2015-11-22 17:00:16 -0500

    added new core data model and VisitedTile entity

---
+++ SeekNYC/VisitedTile+CoreDataProperties.m
@@ -0,0 +1,19 @@
+//
+//  VisitedTile+CoreDataProperties.m
+//  SeekNYC
+//
+//  Created by Justine Gartner on 11/22/15.
+//  Copyright © 2015 Justine Kay. All rights reserved.
+//
+//  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
+//  to delete and recreate this implementation file for your updated model.
+//
+
+#import "VisitedTile+CoreDataProperties.h"
+
+@implementation VisitedTile (CoreDataProperties)
+
+@dynamic columnRow;
+@dynamic timestamp;
+
+@end
