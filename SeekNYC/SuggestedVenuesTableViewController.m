3265ba4c729b928909e332a7a8c8c177ba35871b
cdolm92 <cdolm92@gmail.com>
2015-11-24 16:55:47 -0500

    tvc only displays parks

--- SeekNYC/SuggestedVenuesTableViewController.m
+++ SeekNYC/SuggestedVenuesTableViewController.m
@@ -14,6 +14,7 @@
 @interface SuggestedVenuesTableViewController ()
 
 @property (nonatomic) NSMutableArray *venueResults;
+@property (nonatomic) NSMutableArray *parkResults;
 
 @end
 
@@ -22,21 +23,15 @@
 
 - (void)fetchFourSquareData {
     
+    //     create an url
+    NSURL *foursquaredURL = [NSURL URLWithString:@"https://api.foursquare.com/v2/venues/search?near=ny&categoryId=4bf58dd8d48988d12d941735&v=20150214&m=foursquare&client_secret=OHH5FNLYPFF4CIQ4FI1HVJJT4ERPW1MTVG5ZMU4CBNO0RPRV&client_id=E1D5IIQOKCJTC5RF5FTYJ3PTVLAWDZSXGOIINT3AWP3KNEVV"];
     
-    
-    // create an instagram url
-    NSURL *foursquaredParksURL = [NSURL URLWithString:@"https://api.foursquare.com/v2/venues/search?near=ny&categoryId=4bf58dd8d48988d12d941735&v=20150214&m=foursquare&client_secret=OHH5FNLYPFF4CIQ4FI1HVJJT4ERPW1MTVG5ZMU4CBNO0RPRV&client_id=E1D5IIQOKCJTC5RF5FTYJ3PTVLAWDZSXGOIINT3AWP3KNEVV"];
-    
-    // fetch data from the instagram endpoint and print json response
-    [APIManager GETRequestWithURL:foursquaredParksURL completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+    // fetch data from the endpoint and print json response
+    [APIManager GETRequestWithURL:foursquaredURL completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
         
         NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
         
-      //  NSLog(@"%@", json);
-        
         NSArray *venues = json[@"response"][@"venues"];
-       
-     //   NSLog(@"VENUES HERE %@", venues);
         
         // reset my array
         self.venueResults = [[NSMutableArray alloc] init];
@@ -49,21 +44,60 @@
             
             // add post to array
             [self.venueResults addObject:suggestedVenue];
+            NSLog(@"%@", self.venueResults);
+            
+        }
+        
+        
+        
+          [self.tableView reloadData];
+        
+    }];
+    
+}
+    
+  - (void)fetchFourSquareParkData {
+    
+    NSURL *foursquaredParkURL = [NSURL URLWithString:@"https://api.foursquare.com/v2/venues/search?near=ny&categoryId=4bf58dd8d48988d163941735&v=20150214&m=foursquare&client_secret=OHH5FNLYPFF4CIQ4FI1HVJJT4ERPW1MTVG5ZMU4CBNO0RPRV&client_id=E1D5IIQOKCJTC5RF5FTYJ3PTVLAWDZSXGOIINT3AWP3KNEVV"];
+    
+    // fetch data from the instagram endpoint and print json response
+    [APIManager GETRequestWithURL:foursquaredParkURL completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        
+    NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
+    
+        NSArray *parks = json[@"response"][@"venues"];
+        
+        // reset my array
+        self.parkResults = [[NSMutableArray alloc] init];
+
+        // loop through all json posts
+        
+        
+        for (NSDictionary *venue in parks) {
+            
+            // create new post from json
+            SeekNYCParks *suggestedVenuePark = [[SeekNYCParks alloc] initWithJSON:venue];
+            
+            // add post to array
+            [self.parkResults addObject:suggestedVenuePark];
+            NSLog(@"%@", self.parkResults);
             
         }
         
         [self.tableView reloadData];
+    
     }];
+    
 }
-
-
 
 
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    [self fetchFourSquareData];
+    //[self fetchFourSquareData];
+    [self fetchFourSquareParkData];
+    
     self.tableView.backgroundView = [[UIImageView alloc] initWithImage:
                                      [UIImage imageNamed:@"Gray Background.png"]];
     
@@ -83,8 +117,9 @@
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+   
 
-    return self.venueResults.count;
+    return self.parkResults.count;
 }
 
 
@@ -92,20 +127,35 @@
     
     SuggestedVenuesTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"placesidentifier" forIndexPath:indexPath];
     
+    
     SeekNYCParks *suggestedVenue = self.venueResults[indexPath.row];
+    SeekNYCParks *suggestedVenuePark = self.parkResults[indexPath.row];
+    
     
     cell.placeAddress.text = [NSString stringWithFormat:@"%@", suggestedVenue.address];
     cell.placeAddress.lineBreakMode = NSLineBreakByWordWrapping;
     cell.placeAddress.numberOfLines = 0;
+    cell.placeAddress.text = [NSString stringWithFormat:@"%@", suggestedVenuePark.address];
+    cell.placeAddress.lineBreakMode = NSLineBreakByWordWrapping;
+    cell.placeAddress.numberOfLines = 0;
+
    
     cell.placeName.text = [NSString stringWithFormat:@"%@", suggestedVenue.name];
     cell.placeName.lineBreakMode = NSLineBreakByWordWrapping;
     cell.placeName.numberOfLines = 0;
+    cell.placeName.text = [NSString stringWithFormat:@"%@", suggestedVenuePark.name];
+    cell.placeName.lineBreakMode = NSLineBreakByWordWrapping;
+    cell.placeName.numberOfLines = 0;
+
     
     cell.placeCategory.text = [NSString stringWithFormat:@"%@", suggestedVenue.categoryName];
+    cell.placeCategory.text = [NSString stringWithFormat:@"%@", suggestedVenuePark.categoryName];
     
     return cell;
 }
+
+
+
 
 
 
