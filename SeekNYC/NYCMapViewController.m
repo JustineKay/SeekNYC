d58ca4ce4190f9393dc9cfbf1e8a555420c07e74
cdolm92 <cdolm92@gmail.com>
2015-11-30 21:07:25 -0500

    added landmarks to array

--- SeekNYC/NYCMapViewController.m
+++ SeekNYC/NYCMapViewController.m
@@ -100,7 +100,7 @@ NSFetchedResultsControllerDelegate
 
 @property (nonatomic, strong) NSMutableIndexSet *optionIndices;
 
-@property (nonatomic) NSMutableArray *parkResults;
+@property (nonatomic) NSMutableArray *venueResults;
 
 @end
 
@@ -129,7 +129,7 @@ NSFetchedResultsControllerDelegate
         NSArray *venues = json[@"response"][@"venues"];
         
         // reset my array
-        self.parkResults = [[NSMutableArray alloc] init];
+        self.venueResults = [[NSMutableArray alloc] init];
         
         // loop through all json posts
         for (NSDictionary *venue in venues) {
@@ -138,11 +138,41 @@ NSFetchedResultsControllerDelegate
             SeekNYCParks *suggestedVenue = [[SeekNYCParks alloc] initWithJSON:venue];
             
             // add post to array
-            [self.parkResults addObject:suggestedVenue];
+            [self.venueResults addObject:suggestedVenue];
             //  NSLog(@"%@", self.venueResults);
             
         }
      }];
+    
+}
+
+- (void)fetchLandmarkFourSquareData {
+    
+    //     create an url
+    NSURL *foursquaredURL = [NSURL URLWithString:@"https://api.foursquare.com/v2/venues/search?near=ny&categoryId=4bf58dd8d48988d12d941735&v=20150214&m=foursquare&client_secret=OHH5FNLYPFF4CIQ4FI1HVJJT4ERPW1MTVG5ZMU4CBNO0RPRV&client_id=E1D5IIQOKCJTC5RF5FTYJ3PTVLAWDZSXGOIINT3AWP3KNEVV"];
+    
+    // fetch data from the endpoint and print json response
+    [APIManager GETRequestWithURL:foursquaredURL completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        
+        NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
+        
+        NSArray *venues = json[@"response"][@"venues"];
+        
+//        // reset my array
+//        self.venueResults = [[NSMutableArray alloc] init];
+        
+        // loop through all json posts
+        for (NSDictionary *venue in venues) {
+            
+            // create new post from json
+            SeekNYCParks *suggestedVenue = [[SeekNYCParks alloc] initWithJSON:venue];
+            
+            // add post to array
+            [self.venueResults addObject:suggestedVenue];
+            //  NSLog(@"%@", self.venueResults);
+            
+        }
+    }];
     
 }
      
@@ -153,6 +183,7 @@ NSFetchedResultsControllerDelegate
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self fetchFourSquareData];
+    [self fetchLandmarkFourSquareData];
     
     self.mapView.delegate = self;
     
@@ -1244,18 +1275,20 @@ NSFetchedResultsControllerDelegate
 }
 
 -(IBAction)showAlert {
+
     
-int random = arc4random_uniform((int)self.parkResults.count);
+int random = arc4random_uniform((int)self.venueResults.count);
     
-    
-    
-SeekNYCParks *suggestedVenue = [self.parkResults objectAtIndex:random];
+SeekNYCParks *suggestedVenue = [self.venueResults objectAtIndex:random];
+
+
+
 
 NYAlertViewController *alertShakeGesture = [[NYAlertViewController alloc] initWithNibName:nil bundle:nil];
 
 // Set a title and message
 alertShakeGesture.title = NSLocalizedString(suggestedVenue.name, nil);
-alertShakeGesture.message = NSLocalizedString(@"Description", nil);
+alertShakeGesture.message = NSLocalizedString(suggestedVenue.categoryName, nil);
 
 // Customize appearance as desired
 
