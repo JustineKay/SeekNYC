f300efc645521a7386696df4ab712caca37e8c8b
Felicia Weathers <fel.weathers@gmail.com>
2015-12-05 12:30:14 -0500

    added hidden location class

---
+++ SeekNYC/NYHiddenLocationsViewController.h
@@ -0,0 +1,13 @@
+//
+//  NYHiddenLocationsViewController.h
+//  SeekNYC
+//
+//  Created by Felicia Weathers on 12/5/15.
+//  Copyright © 2015 Justine Kay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NYHiddenLocationsViewController : UIViewController
+
+@end
